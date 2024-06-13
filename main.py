3741071c07669907ccd623a4406d5305b730228d
Renato <renato92.hora@gmail.com>
2024-06-13 17:44:40 -0300

    Rendered titles

---
+++ main.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Arditi SUlce")
+    content = """
+    HHHHHHHHHHHHS   AAAAAAAAAAAAAAA fSSSSSSSSSSSS fffffffffffffffffff
+    """
+    st.info(content)
+
+content2 = """
+ddddddddddddd
+"""
+
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
